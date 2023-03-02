df7e7c0f8037dfa2ec7120207c6f404e5d082085
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-03-02 15:22:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-let ans1 ="";
-let num ="";
-function add(value){
-    console.log(value);
-    num=num+value;
-    document.getElementById('input').value=num;
-
-}
-
-
-function ans(){
-    ans1=eval(num);
-    document.getElementById('input').value=ans1;
-    ans1="";
-    num="";
-}
-
-function clear(){
-    num="";
-    document.getElementById('input').value="";
+let input = document.getElementById("input");
+let expression = "";
+function solve(e) {
+  // console.log(e)
+  if (e === "ans") {
+    // evaluate the expression
+    let result = eval(expression);
+    console.log(result);
+    input.value = result;
+  } else if (e === "clear") {
+    expression = "";
+    input.value = "";
+  } else {
+    expression += e;
+    console.log(expression);
+    input.value = expression;
+  }
 }
